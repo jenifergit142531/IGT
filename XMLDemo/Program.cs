d79bf265b473b621786ae568e974ad372df2a4b1
jenifery <jenifers@revature.com>
2023-12-12 05:48:57 -0500

    4

--- XMLDemo/Program.cs
+++ XMLDemo/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Xml;
+using System.Xml.Schema;
 
 class Program
 {
-    public static XmlElement CreateBookElement(XmlDocument xmldoc, string id, string title, string author)
+    public static XmlElement CreateBookElement(XmlDocument xmldoc, string id, string title, string author,decimal price)
     {
         //create a book element
         XmlElement bookElement = xmldoc.CreateElement("Book");
@@ -25,7 +26,51 @@ class Program
         authorElement.InnerText = author;
         bookElement.AppendChild(authorElement);
 
+        //create price element
+
+        XmlElement priceElement = xmldoc.CreateElement("Price");
+        priceElement.InnerText = price.ToString();
+        bookElement.AppendChild(priceElement);
+
+
         return bookElement;
+    }
+
+    public static void ValidateXmlFile()
+    {
+        try
+        {
+
+            //load xml document
+
+            XmlDocument xmldoc = new XmlDocument();
+             xmldoc.Load("../mybooks.xml");
+
+            //load xml schema
+
+            XmlSchemaSet schemaSet = new XmlSchemaSet();
+            schemaSet.Add(null, "C:\\Users\\JeniferY\\Desktop\\iinterchange\\Console\\DbFIirst\\XMLDemo\\BookStore.xsd");
+
+            //validate the xml document against the xml schema
+            xmldoc.Schemas.Add(schemaSet);
+            xmldoc.Validate(ValidationEventHandler);
+
+            Console.WriteLine("XML is valid");
+        }
+        catch(XmlSchemaValidationException ex)
+        {
+            Console.WriteLine($"validation error :{ex.Message}");
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine($"error : {ex.Message}");
+        }
+    }
+
+    static void ValidationEventHandler(object sender,ValidationEventArgs e)
+    {
+        Console.WriteLine($"Error : {e.Message}");
     }
 
     public static void ReadXml()
@@ -43,16 +88,19 @@ class Program
             string id = bookNode.Attributes["Id"].Value;
             string title = bookNode.SelectSingleNode("Title").InnerText;
             string author = bookNode.SelectSingleNode("Author").InnerText;
+            string price = bookNode.SelectSingleNode("Price").InnerText;
 
             Console.WriteLine("Book Id =>" + id);
             Console.WriteLine("Title =>" + title);
             Console.WriteLine("Author =>" + author);
+            Console.WriteLine("Price =>" + price);
+
         }
     }
 
-    public static void Main(string[] args)
-    {
 
+    public static void createXmlFile()
+    {
         //create xml document
         XmlDocument xmldoc = new XmlDocument();
 
@@ -68,8 +116,10 @@ class Program
 
         //create book element 
 
-        XmlElement bookElement1 = CreateBookElement(xmldoc, "100", "Ikigai", "paul");
-        XmlElement bookElement2 = CreateBookElement(xmldoc, "200", "Inferno", "dan brown");
+        XmlElement bookElement1 = CreateBookElement(xmldoc, "100", "Ikigai", "paul", 150);
+        XmlElement bookElement2 = CreateBookElement(xmldoc, "200", "Inferno", "dan brown", 29);
+
+
 
         //append the book elements to book store / root element
 
@@ -79,9 +129,16 @@ class Program
         xmldoc.Save("../mybooks.xml");
 
         Console.WriteLine("Xml document is created successfully");
+    }
+    public static void Main(string[] args)
+    {
 
 
-        ReadXml();
+        createXmlFile();
+
+        //ReadXml();
+
+        ValidateXmlFile();
 
 
     }
